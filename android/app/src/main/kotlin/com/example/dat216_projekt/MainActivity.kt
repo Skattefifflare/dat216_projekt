8c0ac0db8653b981ac0d32a4f9e0849ac6f98614
Algaras-dev <145165373+Algaras-dev@users.noreply.github.com>
2026-05-08 16:47:14 +0200

    chore: init

---
+++ android/app/src/main/kotlin/com/example/dat216_projekt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dat216_projekt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/model/imat/shopping_item.dart
@@ -0,0 +1,22 @@
+import 'package:dat216_projekt/model/imat/product.dart';
+
+class ShoppingItem {
+  Product product;
+  double amount;
+
+  ShoppingItem(this.product, {this.amount = 1.0});
+
+  double get total => product.price * amount;
+
+  ShoppingItem.fromJson(Map<String, dynamic> json)
+    : product = Product.fromJson(json[_product]),
+      amount = json[_amount];
+
+  Map<String, dynamic> toJson() => {
+    _product: product.toJson(),
+    _amount: amount,
+  };
+
+  static const _product = 'product';
+  static const _amount = 'amount';
+}
