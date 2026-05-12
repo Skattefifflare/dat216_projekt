3f4fd4f64fefd09ed2d252c4cf6ef08e8425e3cc
Algaras-dev <145165373+Algaras-dev@users.noreply.github.com>
2026-05-12 14:02:23 +0200

    feat: add colorScheme

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const double paddingTiny = 4.0;
-  static const double paddingSmall = 8.0;
-  static const double paddingMediumSmall = 12.0;
-  static const double paddingMedium = 16.0;
-  static const double paddingLarge = 24.0;
-  static const double paddingHuge = 32.0;
+  // Widget sizes
+  static const headerHeight = 120.0;
+
+  // Padding
+  static const paddingTiny = 4.0;
+  static const paddingSmall = 8.0;
+  static const paddingMediumSmall = 12.0;
+  static const paddingMedium = 16.0;
+  static const paddingLarge = 24.0;
+
+  static const paddingHuge = 32.0;
 
   static ColorScheme colorScheme = ColorScheme.fromSeed(
     seedColor: Color.fromARGB(255, 108, 173, 130),
+
+    // Override primary
+    primary: Color.fromARGB(255, 108, 173, 130),
   );
 }
