1537ed33c8659ee8b4d33b6f3d074688a2796185
lmguzi <234396440+lmguzi@users.noreply.github.com>
2026-05-14 16:49:25 +0200

    _
    _

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,9 +1,26 @@
+import 'package:dat216_projekt/widgets/home_widgets/category_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widget_previews.dart';
 
 class Profile extends StatelessWidget {
+  @Preview(name: 'My Sample Text')
   const Profile({super.key});
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Container(
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 100,
+            backgroundImage: AssetImage('assets/images/holgerson.png'),
+          ),
+           SizedBox(height: 12),
+          // Name text below like in your design
+           Text(
+            'Hjördis'
+              ),
+            ],
+          ),
+        );
   }
 }
