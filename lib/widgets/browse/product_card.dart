86bc05c9ca39a1b6caa72a4ad85a3f8af5a76c57
Algaras-dev <145165373+Algaras-dev@users.noreply.github.com>
2026-05-20 21:08:24 +0200

    fix: add cards to new grid

--- lib/widgets/browse/product_card.dart
+++ lib/widgets/browse/product_card.dart
@@ -11,40 +11,46 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final imat = context.read<ImatDataHandler>();
-    final image = imat.getImage(product);
+    final iMat = context.read<ImatDataHandler>();
+    final image = iMat.getImage(product);
+
     return Card(
-      margin: .all(AppTheme.categoryCardMargin),
+      margin: const .all(AppTheme.categoryCardMargin),
       clipBehavior: Clip.antiAlias,
-      child: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 60,
-            child: SizedBox(
-              width: double.infinity,
-              height: 120,
-              child: FittedBox(fit: BoxFit.cover, child: image),
-            ),
-          ),
-          Positioned(
-            top: AppTheme.paddingTiny,
-            child: Text(
-              product.name,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: AppTheme.fontLarge,
-                fontWeight: FontWeight.bold,
+
+      child: SizedBox(
+        width: 260,
+        height: 360,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 60,
+              child: SizedBox(
+                width: double.infinity,
+                height: 120,
+                child: FittedBox(fit: BoxFit.cover, child: image),
               ),
             ),
-          ),
-          Positioned(
-            bottom: AppTheme.paddingMedium,
-            right: AppTheme.paddingMedium,
-            child: AddToCart(),
-          ),
-        ],
+            Positioned(
+              top: AppTheme.paddingTiny,
+              child: Text(
+                product.name,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: AppTheme.fontLarge,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Positioned(
+              bottom: AppTheme.paddingMedium,
+              right: AppTheme.paddingMedium,
+              child: AddToCart(),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/browse/product_grid.dart
+++ lib/widgets/browse/product_grid.dart
@@ -1,22 +1,27 @@
 import 'package:dat216_projekt/model/filter_handler.dart';
 import 'package:dat216_projekt/model/imat_data_handler.dart';
+import 'package:dat216_projekt/widgets/browse/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ProductGrid extends StatelessWidget {
   const ProductGrid({super.key});
+
   @override
   Widget build(BuildContext context) {
     final filterState = context.watch<FilterHandler>();
     var iMat = context.read<ImatDataHandler>();
+    print(iMat.selectProducts.length);
 
     return Container(
       alignment: .topCenter,
       child: SingleChildScrollView(
         child: Wrap(
           children: [
-            for (final product in filterState.matchingProducts(iMat.products))
-              SizedBox(width: 150, height: 300, child: Placeholder()),
+            for (final product in filterState.matchingProducts(
+              iMat.selectProducts,
+            ))
+              ProductCard(product: product),
           ],
         ),
       ),
